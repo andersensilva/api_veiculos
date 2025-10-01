c0365b272cf7519953b0a49dbf89bd3d9007926b
Andersen Silva <andersen_silva_18@hotmail.com>
2025-10-01 15:44:43 -0300

    initial project

---
+++ src/main/kotlin/com/wswork/api_veiculos/ApiVeiculosApplication.kt
@@ -0,0 +1,11 @@
+package com.wswork.api_veiculos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiVeiculosApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiVeiculosApplication>(*args)
+}
---
+++ src/test/kotlin/com/wswork/api_veiculos/ApiVeiculosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wswork.api_veiculos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiVeiculosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
